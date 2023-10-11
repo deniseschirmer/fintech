4ef4f5e9353e4d97a9ebc5408ff6d355831ab3e7
deniseschirmer <deniseschirmerr@gmail.com>
2023-10-11 08:04:49 -0300

    feat: added components

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import Resumo from "./Pages/Resumo";
 import "./Style.css";
+import Header from "./components/Header";
+import Sidenav from "./components/Sidenav";
 
 function App() {
-  return <div>Oi</div>;
+  return (
+    <div>
+      <Sidenav />
+      <main>
+        <Resumo />
+        <Header />
+      </main>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Sidenav.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Sidenav = () => {
+  return <div></div>;
+};
+
+export default Sidenav;
