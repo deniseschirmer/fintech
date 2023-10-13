25ad24b6c84332c5fef5c7ea912cbc2bc7cb59c6
deniseschirmer <deniseschirmerr@gmail.com>
2023-10-13 07:41:07 -0300

    feat: ading DateInput/DateRange component

---
+++ src/components/DateRange.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import DateInput from "./DateInput";
+
+const DateRange = () => {
+  const [inicio, setInicio] = React.useState("");
+  const [final, setFinal] = React.useState("");
+  return (
+    <form onSubmit={(e) => e.preventDefault()}>
+      <DateInput
+        label="Inicio"
+        value={inicio}
+        onChange={({ target }) => setInicio(target.value)}
+      />
+      {inicio}
+      <DateInput
+        label="Final"
+        value={final}
+        onChange={({ target }) => setFinal(target.value)}
+      />
+      {final}
+    </form>
+  );
+};
+
+export default DateRange;
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,12 @@
 import React from "react";
-import { useData } from "../Context/DataContext";
+import DateRange from "./DateRange";
 
 const Header = () => {
-  const { data } = useData();
-
-  return <div></div>;
+  return (
+    <div>
+      <DateRange />
+    </div>
+  );
 };
 
 export default Header;
