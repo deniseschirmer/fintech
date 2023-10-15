8d92f6bd4bf048b3544e0a0c83ef3082d4ad29ec
deniseschirmer <deniseschirmerr@gmail.com>
2023-10-15 07:55:09 -0300

    feat: sidenav/vendas added page venda

--- src/App.tsx
+++ src/App.tsx
@@ -4,19 +4,26 @@ import "./Style.css";
 import Header from "./components/Header";
 import Sidenav from "./components/Sidenav";
 import Vendas from "./Pages/Vendas";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Venda from "./Pages/Venda";
 
 function App() {
   return (
-    <DataContextProvider>
-      <div className="container">
-        <Sidenav />
-        <main>
-          <Header />
-          <Resumo />
-          <Vendas />
-        </main>
-      </div>
-    </DataContextProvider>
+    <BrowserRouter>
+      <DataContextProvider>
+        <div className="container">
+          <Sidenav />
+          <main>
+            <Header />
+            <Routes>
+              <Route path="/" element={<Resumo />} />
+              <Route path="/vendas" element={<Vendas />} />
+              <Route path="/vendas/id" element={<Venda />} />
+            </Routes>
+          </main>
+        </div>
+      </DataContextProvider>
+    </BrowserRouter>
   );
 }
 
--- src/Pages/Vendas.tsx
+++ src/Pages/Vendas.tsx
@@ -1,7 +1,18 @@
-import React from "react";
+import { useData } from "../Context/DataContext";
+import VendaItem from "../components/VendaItem";
 
 const Vendas = () => {
-  return <div>Vendas</div>;
+  const { data } = useData();
+  if (data === null) return null;
+  return (
+    <ul>
+      {data.map((venda) => (
+        <li key={venda.id}>
+          <VendaItem venda={venda} />
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default Vendas;
